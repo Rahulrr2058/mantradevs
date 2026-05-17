133cae8b1a3d7a6a33d38944c66275a0d3e5e00f
rahul <rahulrouniyar2021@gmail.com>
2026-05-18 01:22:42 +0545

    fix: stop setup-pages action from auto-injecting basePath

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useState } from 'react';
-import '../styles.css';
+import "../styles.css"
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
